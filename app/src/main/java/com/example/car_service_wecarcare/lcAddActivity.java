2e1ed66edf1b4c46165ab32d0a767a9766b3737c
pasindurukshan <it20198640@my.sliit.lk>
2021-09-25 21:57:01 +0530

    Created Add Operation

--- app/src/main/java/com/example/car_service_wecarcare/lcAddActivity.java
+++ app/src/main/java/com/example/car_service_wecarcare/lcAddActivity.java
@@ -45,6 +45,7 @@ public class lcAddActivity extends AppCompatActivity {
             public void onClick(View v) {
                 insertData();
                 clearAll();
+                //Call clear field function
             }
         });
 
